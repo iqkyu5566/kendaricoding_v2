f57571f0b9626395f23b13339bd26016dfea0212
ramzyganteng <keylaanatasya835@gmail.com>
2022-07-27 09:13:45 -1200

    erwan gabung ahlul

--- application/views/depan/v_isi.php
+++ application/views/depan/v_isi.php
@@ -552,7 +552,7 @@
        <div class="row">
 
          <div class="col-lg-6 ">
-           <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d248.7544784325907!2d122.54125409878692!3d-4.005691969322615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1c18ce95f48233ad!2sKendari%20Coding!5e0!3m2!1sid!2sid!4v1606043371750!5m2!1sid!2sid" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0" allowfullscreen></iframe>
+           <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d248.7544784325907!2d122.54125409878692!3d-4.005691969322615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1c18ce95f48233ad!2sKendari%20Coding!5e0!3m2!1sid!2sid!4v1606043371750!5m2!1sid!2sid" width="550" height="350" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0" allowfullscreen></iframe>
          </div>
 
          <div class="col-lg-6">
